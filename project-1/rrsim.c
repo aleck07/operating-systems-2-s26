a084fd31c312ca15b531f62109a8e1ff9d301071
Alec Krsek <aleckrsek@gmail.com>
2026-04-05 16:13:11 -0700

    Refactor code into other functions

--- project-1/rrsim.c
+++ project-1/rrsim.c
@@ -62,7 +62,7 @@ void parse_command_line(int argc, char **argv)
             p->program[j++] = atoi(token);
             token = strtok(NULL, ",");
         }
-        p->program[j] = 0;
+        p->program[j] = -1;
 
         p->state = READY;
         p->pc = 0;
@@ -98,9 +98,109 @@ int jobs_done()
     return 1;
 }
 
+void check_sleeping(struct queue *q)
+{
+    for (int i = 0; i < num_procs; i++)
+    {
+        if (table[i].state == SLEEPING && table[i].time_asleep_remaining <= 0)
+        {
+            table[i].pc++;
+            if (table[i].program[table[i].pc] == -1)
+            {
+                table[i].state = EXITED;
+                printf("PID %d: Exiting\n", table[i].pid);
+            }
+            else
+            {
+                table[i].time_awake_remaining = table[i].program[table[i].pc];
+                table[i].state = READY;
+                printf("PID %d: Waking up for %d ms\n", table[i].pid, table[i].time_awake_remaining);
+                queue_enqueue(q, &table[i]);
+            }
+        }
+    }
+}
+
+void exit_or_sleep(struct process *p, struct queue *q)
+{
+    p->pc++;
+    if (p->program[p->pc] == -1)
+    {
+        p->state = EXITED;
+        printf("PID %d: Exiting\n", p->pid);
+    }
+    else
+    {
+        p->time_asleep_remaining = p->program[p->pc];
+        p->state = SLEEPING;
+        printf("PID %d: Sleeping for %d ms\n", p->pid, p->time_asleep_remaining);
+    }
+}
+
+/**
+ * @brief Advance the clock to the next sleeping process wakeup if the queue is empty.
+ */
+void advance_clock_if_idle(struct queue *q, int *clock)
+{
+    if (!queue_is_empty(q))
+        return;
+
+    int min_sleep = -1;
+    for (int i = 0; i < num_procs; i++)
+    {
+        if (table[i].state == SLEEPING)
+        {
+            if (min_sleep < 0 || table[i].time_asleep_remaining < min_sleep)
+                min_sleep = table[i].time_asleep_remaining;
+        }
+    }
+    if (min_sleep > 0)
+    {
+        *clock += min_sleep;
+        decrement_sleeping(min_sleep);
+    }
+}
+
+/**
+ * @brief Dequeue the next process and run it for one time slice.
+ */
+void run_next_process(struct queue *q, int *clock)
+{
+    struct process *p = queue_dequeue(q);
+    if (p == NULL)
+        return;
+
+    printf("PID %d: Running\n", p->pid);
+
+    int time_slice = MIN(QUANTUM, p->time_awake_remaining);
+    *clock += time_slice;
+    p->time_awake_remaining -= time_slice;
+    decrement_sleeping(time_slice);
+
+    if (p->time_awake_remaining > 0)
+        queue_enqueue(q, p);
+    else
+        exit_or_sleep(p, q);
+
+    printf("PID %d: Ran for %d ms\n", p->pid, time_slice);
+}
+
+/**
+ * @brief Set up the process table, parse arguments, and enqueue all processes.
+ */
+struct queue *setup(int argc, char **argv)
+{
+    struct queue *q = queue_new();
+    init_proc_table();
+    parse_command_line(argc, argv);
+    for (int i = 0; i < num_procs; i++)
+        queue_enqueue(q, &table[i]);
+    return q;
+}
+
 /**
  * @brief Main function.
- * 
+ *
  * @param argc Argument count
  * @param argv Argument vector
  * @return int Exit status
@@ -108,92 +208,15 @@ int jobs_done()
 int main(int argc, char **argv)
 {
     int clock = 0;
+    struct queue *q = setup(argc, argv);
 
-    struct queue *q = queue_new();
-
-    init_proc_table();
-    parse_command_line(argc, argv);
-
-    for (int i = 0; i < num_procs; i++)
-        queue_enqueue(q, &table[i]);
-
-    while (1)
+    while (!jobs_done())
     {
-        if (jobs_done())
-            break;
-
-        if (queue_is_empty(q))
-        {
-            int min_sleep = -1;
-            for (int i = 0; i < num_procs; i++)
-            {
-                if (table[i].state == SLEEPING)
-                {
-                    if (min_sleep < 0 || table[i].time_asleep_remaining < min_sleep)
-                        min_sleep = table[i].time_asleep_remaining;
-                }
-            }
-            if (min_sleep > 0)
-            {
-                clock += min_sleep;
-                decrement_sleeping(min_sleep);
-            }
-        }
-
+        advance_clock_if_idle(q, &clock);
         printf("=== Clock %d ms ===\n", clock);
-
-        for (int i = 0; i < num_procs; i++)
-        {
-            if (table[i].state == SLEEPING && table[i].time_asleep_remaining <= 0)
-            {
-                table[i].pc++;
-                if (table[i].program[table[i].pc] == 0)
-                {
-                    table[i].state = EXITED;
-                    printf("PID %d: Exiting\n", table[i].pid);
-                }
-                else
-                {
-                    table[i].time_awake_remaining = table[i].program[table[i].pc];
-                    table[i].state = READY;
-                    printf("PID %d: Waking up for %d ms\n", table[i].pid, table[i].time_awake_remaining);
-                    queue_enqueue(q, &table[i]);
-                }
-            }
-        }
-
-        struct process *p = queue_dequeue(q);
-        if (p == NULL)
-            continue;
-
-        printf("PID %d: Running\n", p->pid);
-
-        int time_slice = MIN(QUANTUM, p->time_awake_remaining);
-        clock += time_slice;
-        p->time_awake_remaining -= time_slice;
-        decrement_sleeping(time_slice);
-
-        if (p->time_awake_remaining > 0)
-        {
-            queue_enqueue(q, p);
-        }
-        else
-        {
-            p->pc++;
-            if (p->program[p->pc] == 0)
-            {
-                p->state = EXITED;
-                printf("PID %d: Exiting\n", p->pid);
-            }
-            else
-            {
-                p->time_asleep_remaining = p->program[p->pc];
-                p->state = SLEEPING;
-                printf("PID %d: Sleeping for %d ms\n", p->pid, p->time_asleep_remaining);
-            }
-        }
-
-        printf("PID %d: Ran for %d ms\n", p->pid, time_slice);
+        check_sleeping(q);
+        run_next_process(q, &clock);
     }
+
     queue_free(q);
 }
