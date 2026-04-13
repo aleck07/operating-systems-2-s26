72a030b2bd54d9f69d28a55f99b0d743a9eff871
Alec Krsek <aleckrsek@gmail.com>
2026-04-13 11:20:00 -0700

    Run prettier

--- week3/challenge3-1.c
+++ week3/challenge3-1.c
@@ -3,9 +3,10 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-const int SIZE=1024;  // bytes
+const int SIZE = 1024; // bytes
 
-struct firework {
+struct firework
+{
     int shell_caliber;
     float lift_charge;
     float burst_charge;
@@ -15,8 +16,8 @@ struct firework {
 int main(void)
 {
     // Allocate some space from the OS
-    void *heap = mmap(NULL, SIZE, PROT_READ|PROT_WRITE,
-                      MAP_ANON|MAP_PRIVATE, -1, 0);
+    void *heap = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
+                      MAP_ANON | MAP_PRIVATE, -1, 0);
 
     // Challenge: Store your favorite letter at the byte `heap` points
     // to, then print it with `%c` and also with `%d`.
@@ -35,41 +36,41 @@ int main(void)
 
     // Challenge: Store a float right *after* the int you just stored.
     // Print them both.
-    *(float *)(heap+sizeof(int)) = 7.2;
-    printf("Challenge #4: int = %d, float = %f\n", *(int *)heap, *(float *)(heap+sizeof(int)));
+    *(float *)(heap + sizeof(int)) = 7.2;
+    printf("Challenge #4: int = %d, float = %f\n", *(int *)heap, *(float *)(heap + sizeof(int)));
 
     // Challenge: Store a short (a smallish integer) at byte offset 512
     // in the `heap` area. Then print it with `%d`, along with the int
     // and float you already stored.
-    *(short *)(heap+512) = 15;
-    printf("Challenge #5: int1 = %d, float = %f, short = %d\n", *(int *)heap, *(float *)(heap+sizeof(int)), *(int *)(heap+512));
+    *(short *)(heap + 512) = 15;
+    printf("Challenge #5: int1 = %d, float = %f, short = %d\n", *(int *)heap, *(float *)(heap + sizeof(int)), *(int *)(heap + 512));
 
     // Challenge: Store a struct firework at byte offset 48 in the
     // `heap` area. Make a `struct firework *` initialized to the right
     // position, then fill it with data (with the `->` operator). Then
     // print out the data, and also print out the int, float, and short
     // from above.
-    ((struct firework *)(heap+48))->shell_caliber = 5;
-    ((struct firework *)(heap+48))->lift_charge = 3.5;
-    ((struct firework *)(heap+48))->burst_charge = 4.5;
-    ((struct firework *)(heap+48))->fuse_delay_ms = 1000;
+    ((struct firework *)(heap + 48))->shell_caliber = 5;
+    ((struct firework *)(heap + 48))->lift_charge = 3.5;
+    ((struct firework *)(heap + 48))->burst_charge = 4.5;
+    ((struct firework *)(heap + 48))->fuse_delay_ms = 1000;
 
     printf("Challenge #6: firework: caliber = %d, lift_charge = %f, burst_charge = %f, fuse_delay_ms = %d\n",
-           ((struct firework *)(heap+48))->shell_caliber,
-           ((struct firework *)(heap+48))->lift_charge,
-           ((struct firework *)(heap+48))->burst_charge,
-           ((struct firework *)(heap+48))->fuse_delay_ms);
+           ((struct firework *)(heap + 48))->shell_caliber,
+           ((struct firework *)(heap + 48))->lift_charge,
+           ((struct firework *)(heap + 48))->burst_charge,
+           ((struct firework *)(heap + 48))->fuse_delay_ms);
 
     // Challenge: Store a second struct firework directly after the
     // first one. Initialize and print.
-    ((struct firework *)(heap+48+sizeof(struct firework)))->shell_caliber = 10;
-    ((struct firework *)(heap+48+sizeof(struct firework)))->lift_charge = 7.0;
-    ((struct firework *)(heap+48+sizeof(struct firework)))->burst_charge = 9.0;
-    ((struct firework *)(heap+48+sizeof(struct firework)))->fuse_delay_ms = 2000;
+    ((struct firework *)(heap + 48 + sizeof(struct firework)))->shell_caliber = 10;
+    ((struct firework *)(heap + 48 + sizeof(struct firework)))->lift_charge = 7.0;
+    ((struct firework *)(heap + 48 + sizeof(struct firework)))->burst_charge = 9.0;
+    ((struct firework *)(heap + 48 + sizeof(struct firework)))->fuse_delay_ms = 2000;
 
     printf("Challenge #7: firework: caliber = %d, lift_charge = %f, burst_charge = %f, fuse_delay_ms = %d\n",
-           ((struct firework *)(heap+48+sizeof(struct firework)))->shell_caliber,
-           ((struct firework *)(heap+48+sizeof(struct firework)))->lift_charge,
-           ((struct firework *)(heap+48+sizeof(struct firework)))->burst_charge,
-           ((struct firework *)(heap+48+sizeof(struct firework)))->fuse_delay_ms);
+           ((struct firework *)(heap + 48 + sizeof(struct firework)))->shell_caliber,
+           ((struct firework *)(heap + 48 + sizeof(struct firework)))->lift_charge,
+           ((struct firework *)(heap + 48 + sizeof(struct firework)))->burst_charge,
+           ((struct firework *)(heap + 48 + sizeof(struct firework)))->fuse_delay_ms);
 }
