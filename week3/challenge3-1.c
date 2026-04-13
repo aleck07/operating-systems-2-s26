d84052f2799dddc00bcb624d4de4f15a949ae3b3
Alec Krsek <aleckrsek@gmail.com>
2026-04-13 10:56:54 -0700

    Challenge 3-1 init

---
+++ week3/challenge3-1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+const int SIZE=1024;  // bytes
+
+struct firework {
+    int shell_caliber;
+    float lift_charge;
+    float burst_charge;
+    int fuse_delay_ms;
+};
+
+int main(void)
+{
+    // Allocate some space from the OS
+    void *heap = mmap(NULL, SIZE, PROT_READ|PROT_WRITE,
+                      MAP_ANON|MAP_PRIVATE, -1, 0);
+
+    // Challenge: Store your favorite letter at the byte `heap` points
+    // to, then print it with `%c` and also with `%d`.
+    // TODO
+
+    // Challenge: Store a string at the byte `heap` points to, then
+    // print it.
+    // TODO
+
+    // Challenge: Store an integer at the byte `heap` points to, then
+    // print it.
+    // TODO
+
+    // Challenge: Store a float right *after* the int you just stored.
+    // Print them both.
+    // TODO
+
+    // Challenge: Store a short (a smallish integer) at byte offset 512
+    // in the `heap` area. Then print it with `%d`, along with the int
+    // and float you already stored.
+    // TODO
+
+    // Challenge: Store a struct firework at byte offset 48 in the
+    // `heap` area. Make a `struct firework *` initialized to the right
+    // position, then fill it with data (with the `->` operator). Then
+    // print out the data, and also print out the int, float, and short
+    // from above.
+    // TODO
+
+    // Challenge: Store a second struct firework directly after the
+    // first one. Initialize and print.
+    // TODO
+}
