c95f81af01dc6e1b1b81ecefce40082659ce3169
Alec Krsek <aleckrsek@gmail.com>
2026-05-17 20:33:41 -0700

    remove random voids

--- project-6/block.c
+++ project-6/block.c
@@ -8,7 +8,7 @@
 
 unsigned char *bread(int block_num, unsigned char *block)
 {
-    (void)lseek(image_fd, (off_t)block_num * BLOCK_SIZE, SEEK_SET);
+    lseek(image_fd, (off_t)block_num * BLOCK_SIZE, SEEK_SET);
     ssize_t bytes_read = read(image_fd, block, BLOCK_SIZE);
     if (bytes_read < BLOCK_SIZE)
         memset(block + bytes_read, 0, BLOCK_SIZE - bytes_read);
@@ -17,8 +17,8 @@ unsigned char *bread(int block_num, unsigned char *block)
 
 void bwrite(int block_num, unsigned char *block)
 {
-    (void)lseek(image_fd, (off_t)block_num * BLOCK_SIZE, SEEK_SET);
-    (void)write(image_fd, block, BLOCK_SIZE);
+    lseek(image_fd, (off_t)block_num * BLOCK_SIZE, SEEK_SET);
+    write(image_fd, block, BLOCK_SIZE);
 }
 
 int alloc(void)
